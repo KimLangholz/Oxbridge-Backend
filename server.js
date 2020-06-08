1c4349b004626541aba412bdf095c513a06a9c2a
Wisam Swaidan <wisamswaidan@Wisams-MacBook-Pro.local>
2020-06-08 23:23:36 +0200

    hopefully it is working on local host

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
-﻿require('rootpath')();
-const express = require('express');
-const app = express();
-const cors = require('cors');
+﻿const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+const cors = require('cors');
+const app = express();
+var port = 8080;
+require('dotenv').config();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Environment specific settings
+if (process.env.NODE_ENV === 'production') {
+    port = process.env.PORT;
+    app.use(cors());
+} else {
+    var corsOptions = {
+        origin: "http://localhost:8081"
+    };
+    app.use(cors(corsOptions));
+}
+
+//Middlewares
 app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// use JWT auth to secure the api
-app.use(jwt());
+const db = require("./app/models");
 
-// api routes
-app.use('/users', require('./users/users.controller'));
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database");
+    })
+    .catch(err => {
+        console.log("Cannot connect to database!", err);
+        process.exit();
+    });
 
-// global error handler
-app.use(errorHandler);
+//Routes
+app.get('/', (req, res) => {
+    res.send('Oxbridge API');
+})
+require("./app/routes/race_log.routes")(app);
+require("./app/routes/admin.routes")(app);
+require("./app/routes/user.routes")(app);
+require("./app/routes/race.routes")(app);
+require("./app/routes/team.routes")(app);
+require("./app/routes/race_result.routes")(app);
 
-// start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+app.listen(port, () => {
+    console.log('Server is running on port:' + port);
 });

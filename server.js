091b1196c8f7fc6bb6a6228b937ee8f9f92a3639
Wisam Swaidan <wisamswaidan@Wisams-MacBook-Pro.local>
2020-06-09 00:09:03 +0200

    changes

--- server.js
+++ server.js
@@ -1,51 +1,30 @@
-﻿const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+﻿require('rootpath')();
+const express = require('express');
 const app = express();
-var port = 8080;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 require('dotenv').config();
 
-// Environment specific settings
-if (process.env.NODE_ENV === 'production') {
-    port = process.env.PORT;
-    app.use(cors());
-} else {
-    var corsOptions = {
-        origin: "http://localhost:8081"
-    };
-    app.use(cors(corsOptions));
-}
-
-//Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
 
 const db = require("./app/models");
 
-db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database");
-    })
-    .catch(err => {
-        console.log("Cannot connect to database!", err);
-        process.exit();
-    });
-
-//Routes
-app.get('/', (req, res) => {
-    res.send('Oxbridge API');
-})
-require("./app/routes/race_log.routes")(app);
-require("./app/routes/admin.routes")(app);
-require("./app/routes/user.routes")(app);
-require("./app/routes/race.routes")(app);
-require("./app/routes/team.routes")(app);
-require("./app/routes/race_result.routes")(app);
-
-app.listen(port, () => {
-    console.log('Server is running on port:' + port);
-});

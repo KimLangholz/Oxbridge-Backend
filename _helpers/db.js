b19b0d083229e4ec3f706ecedcc8486b77ff8cec
Wisam Swaidan <wisamswaidan@Wisams-MacBook-Pro.local>
2020-06-11 13:07:20 +0200

    another try

--- _helpers/db.js
+++ _helpers/db.js
@@ -4,6 +4,17 @@ const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnif
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
 mongoose.Promise = global.Promise;
 
-module.exports = {
-    User: require('../users/user.model')
-};
+const db = {};
+
+//console.log("dbConfig: "+config.connectionString);
+
+db.mongoose = mongoose;
+db.url = config.connectionString;
+db.raceLogs = require("../app/models/race_log.model.js")(mongoose);
+db.admins = require("../app/models/admin.model.js")(mongoose);
+db.users = require("../app/models/user.model.js")(mongoose);
+db.races = require("../app/models/race.model.js")(mongoose);
+db.teams = require("../app/models/team.model.js")(mongoose);
+db.results = require("../app/models/race_result.model.js")(mongoose);
+
+module.exports = db;
